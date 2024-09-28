9c047f265bb8e473ecbc8469e90f346cf32a82f7
Shalu-choudhary <choudharyshalu598@gmail.com>
2024-09-28 23:29:52 +0530

    c++

--- array/count_subarray.cpp
+++ array/count_subarray.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 #include<vector>
 using namespace std;
-int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
-    int n = arr.size(); // size of the given array.
+// int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+//     int n = arr.size(); // size of the given array.
     // int cnt = 0; // Number of subarrays:
     // for (int i = 0 ; i < n; i++) { // starting index i
 
@@ -19,22 +19,38 @@ int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
     // return cnt;
 
     //better  solution(o(n^2))
-    int count=0;
+//     int count=0;
+//     for(int i=0;i<n;i++){
+//         int sum=0;
+//         for(int j=i;j<n;j++){
+//             sum+=arr[j];
+//             if(sum==k){
+//                 count++;
+//             }
+//         }
+//     }return count;
+// }
+
+//optimal solution
+int countsum(vector<int> &arr,int k){
+    int n=arr.size();
+    unordered_map<int,int> mpp;
+    mpp[0]=1;
+    int presum=0;
+    int cnt=0;
     for(int i=0;i<n;i++){
-        int sum=0;
-        for(int j=i;j<n;j++){
-            sum+=arr[j];
-            if(sum==k){
-                count++;
-            }
-        }
-    }return count;
+        presum+=arr[i];
+        int remove=presum-k;
+        cnt+=mpp[remove];
+        mpp[presum]+=1;
+
+    }return cnt;
 }
 int main()
 {
     vector<int>arr = {3, 1, 2, 4};
     int k = 6;
-    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    int cnt =countsum(arr, k);
     cout << "The number of subarrays is: " << cnt << "\n";
     return 0;
 }
