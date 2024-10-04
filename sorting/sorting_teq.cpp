59ad4d9891cb398d4b1a9c83bed3ee801d81c4ce
Shalu-choudhary <choudharyshalu598@gmail.com>
2024-10-04 21:49:14 +0530

    c++

--- sorting/sorting_teq.cpp
+++ sorting/sorting_teq.cpp
@@ -13,6 +13,8 @@
 //     }
 // }
 
+// bubble sort
+
 // void bubble_Sort(int arr[], int n){
 //     for(int i=n-1;i>=0;i--){
 //         int didswap=0;
@@ -63,53 +65,99 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> &arr, int low, int mid, int high) {
-    vector<int> temp; // temporary array
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
+// void merge(vector<int> &arr, int low, int mid, int high) {
+//     vector<int> temp; // temporary array
+//     int left = low;      // starting index of left half of arr
+//     int right = mid + 1;   // starting index of right half of arr
 
-    //storing elements in the temporary array in a sorted manner//
+//     //storing elements in the temporary array in a sorted manner//
 
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
-            temp.push_back(arr[left]);
-            left++;
+//     while (left <= mid && right <= high) {
+//         if (arr[left] <= arr[right]) {
+//             temp.push_back(arr[left]);
+//             left++;
+//         }
+//         else {
+//             temp.push_back(arr[right]);
+//             right++;
+//         }
+//     }
+
+//     // if elements on the left half are still left //
+
+//     while (left <= mid) {
+//         temp.push_back(arr[left]);
+//         left++;
+//     }
+
+//     //  if elements on the right half are still left //
+//     while (right <= high) {
+//         temp.push_back(arr[right]);
+//         right++;
+//     }
+
+//     // transfering all elements from temporary to arr //
+//     for (int i = low; i <= high; i++) {
+//         arr[i] = temp[i - low];
+//     }
+// }
+
+// void mergeSort(vector<int> &arr, int low, int high) {
+//     if (low >= high) return;
+//     int mid = (low + high) / 2 ;
+//     mergeSort(arr, low, mid);  // left half
+//     mergeSort(arr, mid + 1, high); // right half
+//     merge(arr, low, mid, high);  // merging sorted halves
+// }
+
+// int main() {
+
+//     vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+//     int n = 7;
+
+//     cout << "Before Sorting Array: " << endl;
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " "  ;
+//     }
+//     cout << endl;
+//     mergeSort(arr, 0, n - 1);
+//     cout << "After Sorting Array: " << endl;
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " "  ;
+//     }
+//     cout << endl;
+//     return 0 ;
+// }
+
+// quick sort
+int partition(vector<int> &arr, int low, int high){
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+    while (i<j)
+    {
+        while (arr[i]<=pivot&&i<=high-1)
+        {i++;
         }
-        else {
-            temp.push_back(arr[right]);
-            right++;
+        while(arr[j]>pivot&&j>=low+1){
+            j--;
         }
+        if(i<j)
+        swap(arr[i],arr[j]);
+        
     }
-
-    // if elements on the left half are still left //
-
-    while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
-    }
-
-    //  if elements on the right half are still left //
-    while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
-    }
-
-    // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
+    swap(arr[low],arr[j]);
+    return j;
+    
+}
+void qs(vector<int>&arr, int low, int high){
+    if(low<high){
+        int pivotinx=partition(arr,low,high);
+        qs(arr,low,pivotinx-1);
+        qs(arr,pivotinx+1,high);
     }
 }
-
-void mergeSort(vector<int> &arr, int low, int high) {
-    if (low >= high) return;
-    int mid = (low + high) / 2 ;
-    mergeSort(arr, low, mid);  // left half
-    mergeSort(arr, mid + 1, high); // right half
-    merge(arr, low, mid, high);  // merging sorted halves
-}
-
-int main() {
-
+int main(){
     vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
     int n = 7;
 
@@ -118,11 +166,12 @@ int main() {
         cout << arr[i] << " "  ;
     }
     cout << endl;
-    mergeSort(arr, 0, n - 1);
+     qs(arr, 0, n - 1);
     cout << "After Sorting Array: " << endl;
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " "  ;
     }
     cout << endl;
-    return 0 ;
+
+    return 0;
 }
