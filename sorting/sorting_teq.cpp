58136e69092c509144fa4d26fc352c91c8bfc55a
Shalu-choudhary <choudharyshalu598@gmail.com>
2024-10-04 10:14:13 +0530

    c++

--- sorting/sorting_teq.cpp
+++ sorting/sorting_teq.cpp
@@ -15,12 +15,30 @@ void selection_sort(int arr[],int n){
 
 void bubble_Sort(int arr[], int n){
     for(int i=n-1;i>=0;i--){
+        int didswap=0;
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j+1];
                 arr[j+1]=arr[j];
                 arr[j]=temp;
+                didswap=1;
             }
+        }
+        if(didswap==0){
+            break;
+        }
+    }
+}
+
+void insertion_sort(int arr[], int n){
+    for(int i=0;i<=n-1;i++){
+        int j=i;
+        while(j>0&&arr[j-1]>arr[j]){
+            int temp=arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+
+            j--;
         }
     }
 }
@@ -30,8 +48,11 @@ int main(){
     int arr[n];
     for(int i=0;i<n;i++)
     cin>>arr[i];
+
     // selection_sort(arr,n);
-    bubble_Sort(arr,n);
+    // bubble_Sort(arr,n);
+    insertion_sort(arr,n);
+
     for(int i=0;i<n;i++)
     cout<<arr[i]<<" ";
     return 0;
