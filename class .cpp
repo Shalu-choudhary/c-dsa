600c255198b3916d87b3d9856ef419d3d747f83b
Shalu-choudhary <choudharyshalu598@gmail.com>
2024-10-14 18:41:01 +0530

    dsa

---
+++ class .cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class human{
+  string name;
+  int age;
+  string loc;
+
+  // constructor--> special fun which take diffrent type of value and assign to the keys
+  // name of the constructor same as name of the class
+
+  human(string n, int a, string l){
+    name=n;
+    age=a;
+    loc=l;
+
+  }
+
+
+};
+int main(){
+
+    return 0;
+}
+
