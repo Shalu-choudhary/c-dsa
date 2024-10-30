6b6fd5803ded55aec8e4e57f4ce7eb380c123b56
Shalu-choudhary <choudharyshalu598@gmail.com>
2024-10-30 17:24:57 +0530

    dsa

--- class .cpp
+++ class .cpp
@@ -1,27 +1,38 @@
 #include<iostream>
+#include <string>
 using namespace std;
 
-class human{
-    public:
-  string name;
-  int age;
-  string loc;
+// class human{
+//     public:
+//   string name;
+//   int age;
+//   string loc;
 
-  // constructor--> special fun which take diffrent type of value and assign to the keys
-  // name of the constructor same as name of the class
+//   // constructor--> special fun which take diffrent type of value and assign to the keys
+//   // name of the constructor same as name of the class
 
-  human(string n, int a, string l){
-    name=n;
-    age=a;
-    loc=l;
-  }
-};
-int main(){
-    human obj("shalu","20","Ajmer");
-    cout<<obj.name<<endl;
-    cout<<obj.age<<endl;
-    cout<<obj.loc<<endl;
+//   human(string n, int a, string l){
+//     name=n;
+//     age=a;
+//     loc=l;
+//   }
+// };
+// int main(){
+//     human obj("shalu","20","Ajmer");
+//     cout<<obj.name<<endl;
+//     cout<<obj.age<<endl;
+//     cout<<obj.loc<<endl;
 
-    return 0;
+//     return 0;
+
+
+int main()
+{
+char s1[6] = "Hello";
+char s2[6] = "World";
+char s3[12] = s1 + " " + s2;
+cout<<s3;
+return 0;
 }
+
 
---
+++ quiz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    // int a=5;
+    // int b=a++;   // 6,5
+    // printf("%d%d",a,b);
+
+    // signed char ch;
+    // ch=128;
+    // printf("%d\n",ch);   // -128
+
+//     int x = 5 * 9 / 3 + 9;
+//     printf("%d",x);
+//     short int i = 20;
+// char c = 97;
+// printf("%d, %d, %d\n", sizeof(i), sizeof(c), sizeof(c + i));
+//     return 0;
+
+
+
+}
